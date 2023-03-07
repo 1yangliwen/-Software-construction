58c9e2d5676d10b44e408f9f20f90cc8e38aa7d6
1yangliwen <2602180247@qq.com>
2023-03-07 23:45:42 +0800

    homework2

---
+++ software construction homework/homework2/IntArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2
+{
+    internal class IntArray
+    {   
+        public void arrayprocess(int[] arr, int max, int min, int ave, int sum)
+        {
+            foreach (int i in arr)
+            {
+                if (i < min) min = i;
+                if (i > max) max = i;
+                sum += i;
+            }
+            ave = sum / arr.Length;
+        }
+    }
+}
---
+++ software construction homework/homework2/array/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class Class1
+    {
+        public bool MatrixJudge(int[][] matrix) { 
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            //检查第一列中除第一个元素外的所有元素所在的对角线
+            for (int i = 1; i < m; i++)
+            {
+                int x = i, y = 0;
+                while (x < m && y < n)
+                {
+                    if (matrix[x][y] != matrix[i - 1][0]) return false;
+                    x++;
+                    y++;                       
+                }
+            }
+            //检查第一行中除第一个元素外的所有元素所在的对角线
+            for (int j = 1; j < n; j++)
+            {
+                int x = 0, y = j;
+                while (x < m && y < n)
+                {
+                    if (matrix[x][y] != matrix[0][j - 1]) return false;
+                    x++;
+                    y++;
+                }
+            }
+            return true;
+        }
+    }
+}
